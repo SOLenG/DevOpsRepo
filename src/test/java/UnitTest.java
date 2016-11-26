6d3a52d4da46d83b674b1f397c7ea8e0b2d8ba10
gabyzaaf <zaafrani.prog@gmail.com>
2016-11-26 15:06:56 +0100

    the official commit

---
+++ src/test/java/UnitTest.java
@@ -0,0 +1,30 @@
+import com.gabriel.id.Webservices;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by zaafranigabriel on 26/11/2016.
+ */
+public class UnitTest {
+
+
+
+
+    @Test
+    public void shouldaddUser(){
+        Webservices webservices = new Webservices();
+        String values = webservices.sendPost("zaafrani","gabriel");
+        Assert.assertTrue(values.contains("OK"));
+    }
+
+
+    @Test
+    public void shouldRetournIdUser(){
+        Webservices webservices = new Webservices();
+        String values = webservices.sendGet("zaafrani","gabriel");
+        Assert.assertTrue(values.contains("zaafrani") && values.contains("gabriel"));
+    }
+
+
+
+}
